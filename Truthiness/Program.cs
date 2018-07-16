ebf188d45de5c59841b60c0def69aec4f969a614
tylerjgabb <tylerjgabb@gmail.com>
2018-07-15 20:56:03 -0700

    Added preliminary logic. More research on types necessary

--- Truthiness/Program.cs
+++ Truthiness/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Truthiness
 {
@@ -6,7 +10,48 @@ namespace Truthiness
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+
+        public static bool IsTruthy(this object o)
+        {
+            if (o == null) return false;
+            var t = o.GetType();
+            if (t.IsPrimitive)
+            {
+                if( t.IsValueType) return DoCascadingTry(new List<Func<bool>>()
+                {
+                    () => Math.Abs(Convert.ToDouble(o)) >= double.Epsilon,
+                    () => Convert.ToChar(o) > 0,
+                });
+            }
+            else
+            {
+                if (t == typeof(string)) return t.ToString().Any();
+
+            }
+                
+            return false;
+        }
+
+        private static T DoCascadingTry<T>(IEnumerable<Func<T>> funcs)
+        {
+            foreach (var f in funcs)
+            {
+                try
+                {
+                    return f.Invoke();
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            return default(T);
         }
     }
 }
